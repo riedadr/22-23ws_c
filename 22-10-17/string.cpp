7986cde3019a9219665dd10af5e7162170b44808
riedadr@eckbert <riedadr@gmail.com>
2022-10-17 12:56:01 +0200

    22-10-17

---
+++ 22-10-17/string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+    string mystring = "Chachacha";
+    std::cout << "my string: " << mystring << std::endl;
+    return 0;
+}
