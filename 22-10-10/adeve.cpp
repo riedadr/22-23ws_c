7001a1af69d6d8e04671c3ba67d2f94ae75dd1ec
riedadr@eckbert <riedadr@gmail.com>
2022-10-17 12:00:55 +0200

    22-10-10

---
+++ 22-10-10/adeve.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int iAdam = 0;
+    int iEva = 1;
+	int iAbel;
+	iAbel = iAdam + iEva;
+    
+    std::cout << iAdam << " und " << iEva << " machen " << iAbel << std::endl;
+    return 0;
+}
